630ed41b793a652776f902edba0922a66a4b1786
Dmitry Dronov <cskafan1911@gmail.com>
2024-02-25 23:02:23 +0300

    Написаны тесты для Lesson.

--- materials/urls.py
+++ materials/urls.py
@@ -13,7 +13,7 @@ router.register('course', CourseViewSet)
 
 urlpatterns = [
                   path('', LessonListView.as_view(), name='lesson_list'),
-                  path('create/', LessonCreateView.as_view(), name='lesson_create'),
+                  path('lesson-create/', LessonCreateView.as_view(), name='lesson_create'),
                   path('lesson-update/<int:pk>/', LessonUpdateView.as_view(), name='lesson_update'),
                   path('lesson-info/<int:pk>/', LessonDetailView.as_view(), name='lesson_detail'),
                   path('lesson-delete/<int:pk>/', LessonDeleteView.as_view(), name='lesson_delete'),
